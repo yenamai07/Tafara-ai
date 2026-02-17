056e8c66ed4655891a05a7a685e5740bc84e409d
yenamai07 <yenamai07@gmail.com>
2026-02-16 16:58:54 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,13 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Tafara.ai - Free & Easy AI for All',
-  description: 'Build your own AI without coding. Private, always free, and accessible to everyone.',
+  description: 'Build your own AI without coding. Private, free, and accessible to everyone.',
   verification: {
     google: 'nj6K2HUiFvac22HiYCUWweH7S2jVwSka5BHNElZTf1o',
+  },
+  icons: {
+    icon: '/favicon.png',
+    apple: '/favicon.png',
   },
 }
 
